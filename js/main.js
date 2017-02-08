e28766a2ac7b06a3d6efd6861f3e2a58a9064ccb
Brit Butler <redline6561@gmail.com>
2017-02-08 10:03:45 -0500

    Playing with template literals.

--- js/main.js
+++ js/main.js
@@ -9,18 +9,81 @@ function switchPlayer () {
   }
 }
 
-function drawBoard () {
-  var greeting = $("#greeting");
+// var cards = [
+//   {
+//     image_url: '',
+//     number: 42,
+//     suit: 'hearts'
+//   },
+//   {
+//     image_url: '',
+//     number: 42,
+//     suit: 'hearts'
+//   },
+//   {
+//     image_url: '',
+//     number: 42,
+//     suit: 'hearts'
+//   },
+//   {
+//     image_url: '',
+//     number: 42,
+//     suit: 'hearts'
+//   }
+// ];
 
-  board.forEach(function (place) {
-    var space = "<div class=\"space\">" + place + "</div>";
-    greeting.after(space);
-  });
+// function cardsTemplate (cards) {
+//   var html = "";
+//   cards.forEach(function (card) {
+//     var cardHTML = "<div class=\"card\"><img class=\"card-image\">" + card.image_url + "</img><p>" + "The " + card.rank + " of " + card.suit + "</p></div>";
+//     html += cardHTML;
+//   })
+//   return html;
+// }
 
-  $(".space").wrapAll("<div class=\"board\">");
+// function boardTemplate (board) {
+//   var html = "";
+//   board.forEach(function (space) {
+//     var spaceHtml = "<div class=\"space\"" + space + "</div>";
+//     html += spaceHtml;
+//   });
+//   return "<div class=\"board\">" + html + "</div>";
+// }
+
+function boardTemplate (board) {
+  var spacesHtml = board.map(function (x) { return `<div class="space">${x}</div>`; });
+  return `
+    <div class="board">
+      ${spacesHtml.join("\n")}
+    </div>
+  `;
 }
 
-drawBoard();
+// function boardTemplate (board) {
+//   var spacesHtml = board.map(function (x) { return `<div class="space">${x}</div>`; });
+//   return `<div class="board">${spacesHtml.join("")}</div>`;
+// }
+
+function render () {
+  var boardHTML = boardTemplate(board);
+  var greeting = $("#greeting");
+  greeting.after(boardHTML);
+}
+
+//render();
+
+// function drawBoard (board) {
+//   var greeting = $("#greeting");
+
+//   board.forEach(function (place) {
+//     var space = "<div class=\"space\">" + place + "</div>";
+//     greeting.after(space);
+//   });
+
+//   $(".space").wrapAll("<div class=\"board\">");
+// }
+
+// drawBoard(board);
 
 function changeBackground (event) {
   var target = $(event.target);
